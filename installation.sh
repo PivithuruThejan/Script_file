5fbceda5c2f31888573de08cd50128d4664e47e4
Pivithuru Amarasinghe <you@example.com>
2019-03-18 13:41:27 +0530

    initial commit

---
+++ installation.sh
@@ -0,0 +1,35 @@
+#! /bin/bash
+sudo yum update -y
+echo "System updated!" >> /home/ec2-user/OADA.log
+
+sudo yum install git -y 
+echo "Git Installed!" >> /home/ec2-user/OADA.log
+
+curl --silent --location https://rpm.nodesource.com/setup_8.x | sudo bash -
+sudo yum -y install nodejs
+echo "Node Installed!" >> /home/ec2-user/OADA.log
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+sudo mount /tmp -o remount,exec
+echo "Docker-Compose Installed!" >> /home/ec2-user/OADA.log
+
+cd /home/ec2-user
+sudo git clone https://github.com/OADA/oada-srvc-docker.git
+echo "OADA Server Cloned!" >> /home/ec2-user/OADA.log
+
+cd oada-srvc-docker/
+sudo docker-compose build
+docker-compose run yarn
+docker-compose up -d
+echo "OADA Server Up!" >> /home/ec2-user/OADA.log
+
+cd /etc/rc.d/
+sudo su
+echo 'cd /home/ec2-user/oada-srvc-docker/' >> rc.local
+echo 'sudo mount /tmp -o remount,exec' >> rc.local
+echo 'docker-compose up -d' >> rc.local
+exit
+sudo chmod +x /etc/rc.d/rc.local
+echo "System updated!" >> /home/ec2-user/OADA.log
